5549109752bcdeb1e4f2622f3c2738f2d717bc3b
Mykola1996 <91569829+Mykola1996@users.noreply.github.com>
2022-01-18 19:05:16 +0200

    cw

---
+++ src/components/Rocket/Rocket.js
@@ -0,0 +1,11 @@
+export default function RocketRoket ({model}) {
+    return (
+        <div className="container">
+            <div className="text">
+                <div className="name">{model.mission_name}</div>
+                <div className="year">{model.launch_year}</div>
+            </div>
+            <img src={model.links.mission_patch} alt="mission_patch"/>
+        </div>
+    );
+}
