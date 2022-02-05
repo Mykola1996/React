faf68d70978cb053e85376ea2355e04057d1eefe
Mykola1996 <91569829+Mykola1996@users.noreply.github.com>
2022-02-05 15:11:13 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer, useRef} from "react";
 
-function App() {
+import {ButtonCats, ButtonDogs, actions, formReducer} from "./components";
+
+
+const App = () => {
+  const [state, dispatch] = useReducer(formReducer, {cats: [], dogs: []});
+  const catInput = useRef();
+  const dogInput = useRef();
+
+  const addCat = (e) => {
+    e.preventDefault();
+    const name = catInput.current.value;
+
+    if (!name) {
+      return
+    }
+
+    const cat = {
+      id: Date.now(),
+      name
+    }
+
+    dispatch({type: actions.ADD_CAT, payload: {cat}})
+    catInput.current.value = ''
+  }
+
+  const addDog = (e) => {
+    e.preventDefault();
+    const name = dogInput.current.value;
+
+    if (!name) {
+      return
+    }
+
+    const dog = {
+      id: Date.now(),
+      name
+    }
+
+    dispatch({type: actions.ADD_DOG, payload: {dog}})
+    catInput.current.value = ''
+  }
+
+  const deleteCat = (id) => {
+    dispatch({type: actions.DEL_CAT, payload: {id}})
+  }
+
+  const deleteDog = (id) => {
+    dispatch({type: actions.DEL_DOG, payload: {id}})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <form style={{padding: '15px', display: 'flex', justifyContent: 'center'}}>
+          <label>Add Cat: <input ref={catInput} type="text" name={'cat'}/></label>
+          <button onClick={addCat}>Save</button>
+          <label>Add Dog: <input ref={dogInput} type="text" name={'dog'}/></label>
+          <button onClick={addDog}>Save</button>
+        </form>
+        <hr/>
+        <div style={{padding: '15px', display: 'flex', justifyContent: 'center', gap: '350px'}}>
+          <ButtonCats cats={state.cats} deleteCat={deleteCat}/>
+          <ButtonDogs dogs={state.dogs} deleteDog={deleteDog}/>
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export * from './Reducer/Reducer';
+export * from './ButtonCats/ButtonCats';
+export * from './ButtonDogs/ButtonDogs';
